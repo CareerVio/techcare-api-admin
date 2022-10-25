179da285e27a1fda0946f60c01c107af9a622c35
Sirichai.p <songzaa@gmail.com>
2022-10-25 18:51:05 +0700

    set proxy false

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url:'https://stickback.careervio.com',
-  proxy: true,
+  proxy: false,
   app: {
     keys: env.array('APP_KEYS'),
   },

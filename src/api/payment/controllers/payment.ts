a72b86a3cf78f4eb2004284d3ef813a83586062c
mynamefaro <info.naifaro@gmail.com>
2023-05-18 20:57:33 +0000

    v1.2.0 Omise and Payment

--- src/api/payment/controllers/payment.ts
+++ src/api/payment/controllers/payment.ts
@@ -5,16 +5,13 @@ import Omise from 'omise'
 export default factories.createCoreController('api::payment.payment' , ({strapi}) => ({
     async create(ctx) {
         try {
-        console.log("Start Payment ..");
+          
           const { id } = ctx.state.user;
           let { data } = ctx.request.body;
 
           data["user"] = id;
           
-          const omise = Omise({
-            'secretKey': 'skey_test_5vkqume3e8cmwtfh3he',
-            'omiseVersion': '2019-05-29'
-          });
+          
           const charge = {
             'description': data.description,
             'amount': data.amount * 100, 
@@ -22,19 +19,13 @@ export default factories.createCoreController('api::payment.payment' , ({strapi}
             'capture': true,
             'card': data.token
           };
+
+          const response = await strapi.service('api::payment.payment').omise(charge);
+          console.log(response);
+          await strapi.entityService.create('api::payment.payment',{ data });
           
-          var isChagesCreated:boolean = false;
-          await omise.charges.create(charge, function(err, resp) {
-            if (resp.paid) {
-                isChagesCreated = true;
-            } else{
-                //Error
-            }
-          });
-
-          const response = await strapi.entityService.create('api::payment.payment',{ data });
-
           ctx.body = response;
+  
 
         } catch (err) {
           ctx.badRequest(err);
--- src/api/payment/services/payment.ts
+++ src/api/payment/services/payment.ts
@@ -3,5 +3,15 @@
  */
 
 import { factories } from '@strapi/strapi';
+import Omise from 'omise';
 
-export default factories.createCoreService('api::payment.payment');
+export default factories.createCoreService('api::payment.payment', ({strapi}) => ({
+    async omise(charge){
+        const omise = Omise({
+            'secretKey': 'skey_test_5vkqume3e8cmwtfh3he',
+            'omiseVersion': '2019-05-29'
+        });
+        const omise_response = await omise.charges.create(charge);
+        return omise_response;
+    },
+}));

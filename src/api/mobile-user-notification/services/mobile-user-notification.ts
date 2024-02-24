f9ef3bcbc286863633e7a1ca626b762b996f7fe3
flagrantii <tanuson679@gmail.com>
2024-02-24 16:35:20 +0700

    Add no.17 Collections(Notification)

---
+++ src/api/mobile-user-notification/services/mobile-user-notification.ts
@@ -0,0 +1,7 @@
+/**
+ * mobile-user-notification service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mobile-user-notification.mobile-user-notification');
---
+++ src/api/pickup-drop-service/routes/pickup-drop-service.ts
@@ -0,0 +1,7 @@
+/**
+ * pickup-drop-service router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::pickup-drop-service.pickup-drop-service');

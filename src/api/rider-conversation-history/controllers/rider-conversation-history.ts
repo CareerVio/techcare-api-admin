c33251953d3c62f9a593af7428ad6b5318462790
punpunkeshin05 <punpunsirawit@gmail.com>
2024-02-24 23:58:49 +0700

    feat: add rider-user part

---
+++ src/api/rider-conversation-history/controllers/rider-conversation-history.ts
@@ -0,0 +1,7 @@
+/**
+ * rider-conversation-history controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::rider-conversation-history.rider-conversation-history');
---
+++ src/api/rider-user/controllers/rider-user.ts
@@ -0,0 +1,7 @@
+/**
+ * rider-user controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::rider-user.rider-user');
